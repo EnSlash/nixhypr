20aa45325ba80018b238c2660c40135b2faf2e2f
Igor Ershov <I.Ershov@compassplus.com>
2025-12-18 08:29:03 +0500

    Update v1

--- hyprland/hyprland.nix
+++ hyprland/hyprland.nix
@@ -2,16 +2,14 @@
 
 {
   # Enable Hyprland
-  programs.hyprland = {
+  wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
 
   # Enable xdg-desktop-portal-hyprland
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  };
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
 
   # Set environment variables for Wayland
   home.sessionVariables = {
