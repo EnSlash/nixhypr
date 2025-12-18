c17b5c07224b7dcb65cc609872ddb22246cf88f4
Igor Ershov <I.Ershov@compassplus.com>
2025-12-18 08:30:47 +0500

    Update v1

--- home/files.nix
+++ home/files.nix
@@ -1,13 +1,6 @@
 { ... }:
 
 {
-  home.file.".config/i3/config".source = ../i3/config;
-  home.file.".config/i3/desk.jpg".source = ../i3/desk.jpg;
-  home.file.".config/polybar/config.ini".source = ../polybar/config.ini;
-  home.file.".config/polybar/launch.sh" = {
-    source = ../polybar/launch.sh;
-    executable = true;
-  };
   home.file.".bashrc".source = ../configs/.bashrc;
   home.file.".tmux.conf".source = ../configs/.tmux.conf;
 
