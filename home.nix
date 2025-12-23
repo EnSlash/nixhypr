92f7660f93f3ed10556b6562d05faaacd5b2a0c3
Igor Ershov <I.Ershov@compassplus.com>
2025-12-23 09:12:07 +0500

    Update v1

--- home.nix
+++ home.nix
@@ -20,7 +20,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "25.11"; # Please change this to your version of Home Manager
+  home.stateVersion = "25.05"; # Please change this to your version of Home Manager
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
