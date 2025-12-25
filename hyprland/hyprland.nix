6c7b91338d74902cf27a0d54e2eb3036d7e5ea91
Igor Ershov <I.Ershov@compassplus.com>
2025-12-25 11:04:36 +0500

    Update v1

--- hyprland/hyprland.nix
+++ hyprland/hyprland.nix
@@ -23,6 +23,9 @@ let
     echo "/* Custom color override by Nix Agent */" >> $out/src/style.css
     echo "window#waybar { color: #FFFFFF; }" >> $out/src/style.css
 
+    # Patch colorpicker script to avoid empty color value
+    sed -i '/text="$(head -n 1 "$loc\/colors")"/a \[ -z "$text" ] \&\& text="#FFFFFF"' $out/src/scripts/colorpicker.sh
+
     # Overwrite myupdate.sh with a NixOS compatible version
     cat > $out/src/scripts/myupdate.sh << EOF
 #!/bin/sh
